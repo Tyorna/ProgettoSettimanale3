387bd00ecdc2fb68f9ca67a74c973395ad332604
Tyorna <francescaarata@live.it>
2023-07-14 11:35:37 +0200

    base

--- src/main/java/esercizio/Prestito.java
+++ src/main/java/esercizio/Prestito.java
@@ -1,5 +1,43 @@
 package esercizio;
 
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "riviste")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Prestito {
+
+	@Id
+	protected String utente;
+	protected String elementoPrestato;
+	protected LocalDate dataInizioPrestito;
+	protected LocalDate dataRestituzione;
+	protected LocalDate dataRestituzioneEffettiva;
+
+	public Prestito(String utente, String elementoPrestato, LocalDate dataInizioPrestito, LocalDate dataRestituzione,
+			LocalDate dataRestituzioneEffettiva) {
+		this.utente = utente;
+		this.elementoPrestato = elementoPrestato;
+		this.dataInizioPrestito = dataInizioPrestito;
+		this.dataRestituzione = dataRestituzione;
+		this.dataRestituzioneEffettiva = dataRestituzioneEffettiva;
+	}
+
+	@Override
+	public String toString() {
+		return "Prestito [utente=" + utente + ", elemento Prestato =" + elementoPrestato + ", data Inizio Prestito="
+				+ dataInizioPrestito + ", dataRestituzione=" + dataRestituzione + ", data di Restituzione="
+				+ dataRestituzione + "]" + "\n";
+	}
 
 }
--- src/main/java/esercizio/Utente.java
+++ src/main/java/esercizio/Utente.java
@@ -1,5 +1,39 @@
 package esercizio;
 
-public class Utente {
+import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "utenti")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Utente {
+		protected String nome;
+		protected String cognome;
+		protected LocalDate dataNascita;
+		@Id
+		@GeneratedValue
+		protected Long nTessera;
+
+		public Utente(String nome, String cognome, LocalDate dataNascita, Long nTessera) {
+			this.nome = nome;
+			this.cognome = cognome;
+			this.dataNascita = dataNascita;
+			this.nTessera = nTessera;
+		}
+
+		@Override
+		public String toString() {
+			return "Utente [nome=" + nome + ", cognome=" + cognome + ", data di nascita=" + dataNascita
+					+ ", numero Tessera=" + nTessera + "]" + "\n";
+		}
 }
