c2eabe83aa90fa4a71fcfe4f6c58af85eafe8a3e
Tyorna <francescaarata@live.it>
2023-07-14 12:19:27 +0200

    catalogofunzionante

--- src/main/java/esercizio/Archivio.java
+++ src/main/java/esercizio/Archivio.java
@@ -1,10 +1,40 @@
 package esercizio;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import DAO.LibriDAO;
+import DAO.RivisteDAO;
+import util.JpaUtil;
+
 public class Archivio {
+
+	private static EntityManagerFactory emf = JpaUtil.getEntityManagerFactory();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		EntityManager em = emf.createEntityManager();
+		LibriDAO ld = new LibriDAO(em);
+		RivisteDAO rd = new RivisteDAO(em);
+
+		Libri libro1 = new Libri("Titolo1", "Autore1", "fantasy", "12kdk92", 2019, 305);
+		Riviste riviste1 = new Riviste("Riv3", Type.Semestrale, "123jcnaks753", 2010, 115);
+		Libri libro2 = new Libri("Titolo2", "Autore2", "Horror", "456jgfd567", 2021, 295);
+		Riviste riviste2 = new Riviste("Riv2", Type.Mensile, "12asd3", 2015, 137);
+		ld.save(libro1);
+		ld.save(libro2);
+		rd.save(riviste1);
+		rd.save(riviste2);
+
+//		User u = ud.findById(4);
+//		System.out.println(u.getDocument());
+//
+//		Document d = dd.findById(3);
+//		System.out.println(d.getUser());
+
+		em.close();
+		emf.close();
 	}
 
 }
--- src/main/java/esercizio/Catalogo.java
+++ src/main/java/esercizio/Catalogo.java
@@ -1,8 +1,10 @@
 package esercizio;
 
+import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -11,6 +13,8 @@ import lombok.Setter;
 
 @Entity
 @Table(name = "catalogo")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "tipologia")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -18,11 +22,11 @@ import lombok.Setter;
 public abstract class Catalogo {
 
 	@Id
-	@GeneratedValue
 	protected String ISBN;
 	protected String title;
 	protected int annoPubbl;
 	protected int numeroPagine;
+
 
 	public Catalogo(String ISBN, String title, int annoPubbl, int numeroPagine) {
 		this.ISBN = ISBN;
